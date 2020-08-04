5c76f5d235db0b251307789dbe183ea910f4a28a
Alireza Ayinmehr <Alireza.ayinmehr@gmail.com>
2020-08-04 14:39:01 +0430

    Add description to setup

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name="TeleSendTime",
     version='0.1.0',
+    description="Telegram Message Automation Module.",
     url="https://gitlab.com/DarkSuniuM/TeleSendTime",
     project_urls={
         "Code": "https://gitlab.com/DarkSuniuM/TeleSendTime",
